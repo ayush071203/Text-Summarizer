b4e87677b5c449c5a81ef3319a78a1195fba2979
Ayush <133644378+ayush071203@users.noreply.github.com>
2024-06-08 15:05:12 +0530

    Add files via upload

---
+++ Django api/authenticate/textsummarizer/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path , include
+from app.views import home 
+from django.contrib.auth import views as auth_views
+from app import views
+
+urlpatterns = [
+    path('home/',home,name='home'),
+    path('accounts/' , include('django.contrib.auth.urls')),
+    path('accounts/register',views.REGISTER, name = 'register'),
+    path('', views.dologin,name ='dologin'),
+]
